aee88e0a63cbdcd75fd80b65f36b6ee37f99ba43
Rafael Pinheiro <moriarty.pinheiro@gmail.com>
2012-01-24 23:59:36 -0200

    changed data structure, removed description validation and added style

--- js/main.js
+++ js/main.js
@@ -1,25 +1,45 @@
 (function($) {
+    /*
+    {
+        length: 1,
+        todo: [
+            {
+                title: 'test',
+                description: 'test'
+            }
+        ],
+        done: [
+            {
+                title: 'test1',
+                description: 'test2'
+            }
+        ]
+    }
+    */
     function Storage(storage) {
         this.storage = storage || localStorage;
         this.key = new Date().toDateString();
         if (this.storage.getItem(this.key) === null) {
-            this.storage.setItem(this.key, JSON.stringify({}));
+            this.storage.setItem(this.key, JSON.stringify({todo:[], done:[]}));
         }
     }
 
     Storage.prototype = {
-        set: function(key, value) {
+        add: function(value, type) {
+            var type = type || 'todo';
             var items = JSON.parse(this.storage.getItem(this.key));
-            items[key] = value;
+            items[type].push(value);
             this.storage.setItem(this.key, JSON.stringify(items));
-            return true;//TODO: VALIDATION FOR OVERRRIDE
+            return true;
         },
-        get: function(key) {
-            var items = JSON.parse(this.storage.getItem(this.key));
-            return items[key];
+        get: function(index, type) {
+            var type = type || 'todo';
+            var items = JSON.parse(this.storage.getItem(this.key))[type];
+            return items[index];
         },
-        getAll: function() {
-            return JSON.parse(this.storage.getItem(this.key));
+        getAll: function(type) {
+            var type = type || 'todo';
+            return JSON.parse(this.storage.getItem(this.key))[type];
         },
         load: function(key) {
             this.key = key;
@@ -31,6 +51,27 @@
         this.element = $('#new-task');
         this.bindEvents();
     }
+    
+    TaskForm.prototype = {
+        bindEvents: function() {
+            var self = this;
+            this.element.bind('submit', function(ev) {
+                ev.preventDefault();
+                self.save();
+                return false;
+            });
+        },
+        save: function() {
+            var title = this.element.find('#title').val();
+            var description = this.element.find('#description').val();
+            if (title.trim() !== '') {
+                if (storage.add({title:title, description:description})) {
+                    this.element.get(0).reset();
+                    list.refresh();
+                }
+            }
+        }
+    };
     
     function FluxCapacitor() {
         this.element = $('#flux-capacitor');
@@ -54,27 +95,6 @@
         }
     };
     
-    TaskForm.prototype = {
-        bindEvents: function() {
-            var self = this;
-            this.element.bind('submit', function(ev) {
-                ev.preventDefault();
-                self.save();
-                return false;
-            });
-        },
-        save: function() {
-            var title = this.element.find('#title').val();
-            var description = this.element.find('#description').val();
-            if (title.trim() !== '' && description.trim() !== '') {
-                if (storage.set(title, description)) {
-                    this.element.get(0).reset();
-                    list.refresh();
-                }
-            }
-        }
-    };
-    
     function TaskList() {
         this.element = $('#task-list');
         this.load();
@@ -88,13 +108,15 @@
             var items = storage.getAll();
             var html = '';
             $.each(items, function(i) {
-                html += '<li><p class="title">' + i + '</p><p class="description">' + items[i] + '</p></li>'
+                html += '<li><p class="title">' + items[i].title + '</p><p class="description">' + items[i].description.replace('\n', '<br />') + '</p></li>'
             });
             this.element.html(html);
         }
     };
+    
     var storage = new Storage();
     var list = new TaskList();
     var form = new TaskForm();
     var fluxCapacitor = new FluxCapacitor();
+
 })(jQuery);
